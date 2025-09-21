f18eba6ff29761309e8ce8fd31be78a1373ceb83
joseph okorie <tp-ver-jo@ramsayhealth.com.au>
2025-09-21 11:04:12 +1000

    creating dns server

--- lab2/bastion.tf
+++ lab2/bastion.tf
@@ -4,7 +4,7 @@
 
 
 resource "azurerm_public_ip" "bastion" {
-  for_each            = { for key, value in local.hub_subnets : key => value if value.name == "AzureBastionSubnet" }
+  for_each            = { for key, value in local.spoke_subnets : key => value if value.name == "AzureBastionSubnet" && var.environment_name == "dev" }
   name                = "pip-${var.application_name}-${var.environment_name}-bastion"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
@@ -13,14 +13,14 @@ resource "azurerm_public_ip" "bastion" {
 }
 
 resource "azurerm_bastion_host" "main" {
-  for_each            = { for key, value in local.hub_subnets : key => value if value.name == "AzureBastionSubnet" }
+  for_each            = { for key, value in local.spoke_subnets : key => value if value.name == "AzureBastionSubnet" && var.environment_name == "dev" }
   name                = "bas-${var.application_name}-${var.environment_name}"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.hub_subnets[each.key].id
+    subnet_id            = azurerm_subnet.spoke_subnets[each.key].id
     public_ip_address_id = azurerm_public_ip.bastion[each.key].id
   }
 }
